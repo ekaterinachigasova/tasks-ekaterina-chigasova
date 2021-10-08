125acddc5851ed44c9001467e86c731006f75616
ekaterinachigasova <91914720+ekaterinachigasova@users.noreply.github.com>
2021-10-08 20:52:17 +0300

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,9 @@
+const firstName = "Ekaterina"
+const dayOfBirth = "28"
+const monthOfBirth = "09"
+const yearOfBirth = "1997"
+const hobbie = "horse riding, reading"
+const about = {firstName, dayOfBirth, monthOfBirth, yearOfBirth, hobbie}
+console.log(dayOfBirth,monthOfBirth,yearOfBirth)
+console.log(about)
+
