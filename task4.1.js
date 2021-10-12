9169f698d7bd7c3ff1acb9fbb68601fe69728e6b
ekaterinachigasova <91914720+ekaterinachigasova@users.noreply.github.com>
2021-10-12 17:50:30 +0300

    Add files via upload

---
+++ task4.1.js
@@ -0,0 +1,9 @@
+const colors = ["Red", "Green", "White", "Black"]
+
+const a=colors.toString()
+const b=colors.join(", ")
+const c=colors.join(" + ")
+
+console.log(a)
+console.log(b)
+console.log(c)
