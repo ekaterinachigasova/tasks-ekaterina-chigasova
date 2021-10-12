f8e22dc5d54010aa6289128e8c3a558cfce8f15f
ekaterinachigasova <91914720+ekaterinachigasova@users.noreply.github.com>
2021-10-12 17:08:55 +0300

    Add files via upload

---
+++ task2.1.js
@@ -0,0 +1,16 @@
+let Name ="Ekaterina"
+let bday = 28
+let bmonth = 09
+let byear = 1997
+
+const birthday = new Date( "September 28, 1997, 05:45")
+
+const hobbie = ["horse riding", "reading"]
+
+const object = {
+    firstName: Name,
+    dayOfBirth: birthday,
+    hobbie: hobbie
+}
+
+console.log(object)
